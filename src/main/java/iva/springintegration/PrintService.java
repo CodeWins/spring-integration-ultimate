013c542884761cbec39268729cf09aa821e6c94d
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-03-02 22:25:08 -0500

    Added direct channel

---
+++ src/main/java/iva/springintegration/PrintService.java
@@ -0,0 +1,18 @@
+package iva.springintegration;
+
+import java.util.Map.Entry;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
+public class PrintService {
+	
+	public void print (Message<String> message) {
+		System.out.println(message.getPayload());
+		MessageHeaders headers = message.getHeaders();
+		for(Entry<String,Object> e:headers.entrySet()) {
+		System.out.println(e.getKey()+"  "+e.getValue());
+		}
+	}
+
+}
---
+++ src/main/java/iva/springintegration/SpringIntegrationApplication.java
@@ -0,0 +1,40 @@
+package iva.springintegration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.GenericMessage;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationApplication implements ApplicationRunner {
+
+	@Autowired
+	private DirectChannel channel;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Message<String> message = MessageBuilder.withPayload("Hello Payload from Builder")
+				.setHeader("Header1", "Header Value 1").build();
+		PrintService service = new PrintService();
+		service.print(message);
+
+	}
+
+}
